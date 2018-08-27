a8d0be79c24d0b3905197021b94997b88dcc7ac9
svacaortega <svacaortega@gmail.com>
2018-08-27 17:02:36 -0500

    Inicial

---
+++ src/main/java/com/eglobal/siac/server/SiacEurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.eglobal.siac.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@RefreshScope
+public class SiacEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiacEurekaServerApplication.class, args);
+	}
+}
